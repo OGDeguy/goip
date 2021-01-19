de913fb0476f05e1b7b9888e50ae4beead88b8ae
Rylan Merritt <rmerritt@sageisg.com>
2021-01-18 17:45:32 -0700

    Added nesting back in

--- goip/goip_test.go
+++ goip/goip_test.go
@@ -2,20 +2,21 @@ package goip
 
 import (
 	"fmt"
+	"goip"
 )
 
 func ExampleNewCIDR () {
-	cidr, _ := NewCIDR("192.168.1.0/24")
+	cidr, _ := goip.NewCIDR("192.168.1.0/24")
 	fmt.Println(len(cidr.Hosts))
 	// Output: 254
 }
 
 func ExampleIsPrivate() {
-	fmt.Println(IsPrivate("192.168.0.1"))
+	fmt.Println(goip.IsPrivate("192.168.0.1"))
 	// Output: true
 }
 
 func ExampleIsCIDR() {
-	fmt.Println(IsCIDR("192.168.0.0/24"))
+	fmt.Println(goip.IsCIDR("192.168.0.0/24"))
 	// Output: true
 }
